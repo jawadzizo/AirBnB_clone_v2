3bd6b3a165caca80fde981e42a4c27fe312ab3ef
jawadzizo <jadosss99@gmail.com>
2024-08-17 13:57:48 -0700

    use .format instead of f-strings

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ from invoke.context import Context
 def do_pack():
     """packs web_static directory in a tgz file"""
     c = Context()
-    tar_name = f'web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}.tgz'
+    tar_name = 'web_static_{}.tgz'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
 
     c.run("mkdir -p versions")
     with c.cd("web_static"):
@@ -17,8 +17,8 @@ def do_pack():
     if result.failed:
         return None
 
-    c.run(f"mv web_static/{tar_name} versions")
-    full_path = c.run(f"realpath versions/{tar_name}", hide=True)
+    c.run("mv web_static/{} versions".format(tar_name))
+    full_path = c.run("realpath versions/{}".format(tar_name), hide=True)
     index = full_path.stdout.find("versions")
     short_path = full_path.stdout[index:]
 
